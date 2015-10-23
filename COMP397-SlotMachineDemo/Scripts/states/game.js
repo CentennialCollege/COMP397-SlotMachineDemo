987c1106be59b3d21d97197b7ceca615d095ec66
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-10-23 17:56:39 -0400

    added buttons to UI

--- COMP397-SlotMachineDemo/Scripts/states/game.js
+++ COMP397-SlotMachineDemo/Scripts/states/game.js
@@ -14,29 +14,43 @@ var states;
         }
         // PUBLIC METHODS
         Game.prototype.start = function () {
-            // level label
-            this._levelLabel = new objects.Label("Game Play", "60px Consolas", "#000000", 320, 240);
-            this.addChild(this._levelLabel); // add label to the stage
-            // back button
-            this._backButton = new objects.Button("BackButton", 220, 340);
-            this._backButton.on("click", this._clickBackButton, this); // event listener
-            this.addChild(this._backButton);
-            // next button
-            this._nextButton = new objects.Button("NextButton", 420, 340);
-            this._nextButton.on("click", this._clickNextButton, this); // event listener
-            this.addChild(this._nextButton);
+            this._slotMachine = new createjs.Container();
+            this._slotMachine.x = 132.5;
+            this._background = new createjs.Bitmap(assets.getResult("background"));
+            this._slotMachine.addChild(this._background); // add background image
+            this._bet1Button = new objects.SpriteButton("bet1Button", 23, 386);
+            this._slotMachine.addChild(this._bet1Button);
+            this._bet10Button = new objects.SpriteButton("bet10Button", 88, 386);
+            this._slotMachine.addChild(this._bet10Button);
+            this._bet100Button = new objects.SpriteButton("bet100Button", 153, 386);
+            this._slotMachine.addChild(this._bet100Button);
+            this._betMaxButton = new objects.SpriteButton("betMaxButton", 218, 386);
+            this._slotMachine.addChild(this._betMaxButton);
+            this._spinButton = new objects.SpriteButton("spinButton", 289, 386);
+            this._slotMachine.addChild(this._spinButton);
+            this._tile1 = new objects.GameObject("blank", 74, 192);
+            this._slotMachine.addChild(this._tile1);
+            this._tile2 = new objects.GameObject("blank", 152, 192);
+            this._slotMachine.addChild(this._tile2);
+            this._tile3 = new objects.GameObject("blank", 230, 192);
+            this._slotMachine.addChild(this._tile3);
+            this._betLine = new objects.GameObject("bet_line", 61, 225);
+            this._slotMachine.addChild(this._betLine);
+            this.addChild(this._slotMachine);
             stage.addChild(this);
+            // add event listeners
+            this._bet1Button.on("click", this._clickBet1Button, this);
+            this._spinButton.on("click", this._spinButtonClick, this);
         };
         Game.prototype.update = function () {
         };
         // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++++++++
         // Callback function / Event Handler for Back Button Click
-        Game.prototype._clickBackButton = function (event) {
-            changeState(config.MENU_STATE);
+        Game.prototype._clickBet1Button = function (event) {
+            console.log("bet 1");
         };
-        // Callback function / Event Handler for Next Button Click
-        Game.prototype._clickNextButton = function (event) {
-            changeState(config.OVER_STATE);
+        //WORKHORSE OF THE GAME
+        Game.prototype._spinButtonClick = function (event) {
         };
         return Game;
     })(objects.Scene);
--- COMP397-SlotMachineDemo/Scripts/states/game.ts
+++ COMP397-SlotMachineDemo/Scripts/states/game.ts
@@ -2,9 +2,19 @@
     // GAME CLASS
     export class Game extends objects.Scene {
         // PRIVATE INSTANCE VARIABLES
-        _levelLabel: objects.Label;
-        _backButton: objects.Button;
-        _nextButton: objects.Button;
+        private _slotMachine: createjs.Container;
+        private _background: createjs.Bitmap;
+        private _bet1Button: objects.SpriteButton;
+        private _bet10Button: objects.SpriteButton;
+        private _bet100Button: objects.SpriteButton;
+        private _betMaxButton: objects.SpriteButton;
+        private _spinButton: objects.SpriteButton;
+
+        private _tile1: objects.GameObject;
+        private _tile2: objects.GameObject;
+        private _tile3: objects.GameObject;
+
+        private _betLine: objects.GameObject;
 
         // CONSTRUCTOR
         constructor() {
@@ -13,22 +23,46 @@
 
         // PUBLIC METHODS
         public start(): void {
+            this._slotMachine = new createjs.Container();
+            this._slotMachine.x = 132.5;
 
-            // level label
-            this._levelLabel = new objects.Label("Game Play", "60px Consolas", "#000000", 320, 240, true);
-            this.addChild(this._levelLabel); // add label to the stage
+            this._background = new createjs.Bitmap(assets.getResult("background"));
+            this._slotMachine.addChild(this._background); // add background image
 
-            // back button
-            this._backButton = new objects.Button("BackButton", 220, 340);
-            this._backButton.on("click", this._clickBackButton, this); // event listener
-            this.addChild(this._backButton);
+            this._bet1Button = new objects.SpriteButton("bet1Button", 23, 386);
+            this._slotMachine.addChild(this._bet1Button);
 
-            // next button
-            this._nextButton = new objects.Button("NextButton", 420, 340);
-            this._nextButton.on("click", this._clickNextButton, this); // event listener
-            this.addChild(this._nextButton);
+            this._bet10Button = new objects.SpriteButton("bet10Button", 88, 386);
+            this._slotMachine.addChild(this._bet10Button);
 
+            this._bet100Button = new objects.SpriteButton("bet100Button", 153, 386);
+            this._slotMachine.addChild(this._bet100Button);
+
+            this._betMaxButton = new objects.SpriteButton("betMaxButton", 218, 386);
+            this._slotMachine.addChild(this._betMaxButton);
+
+            this._spinButton = new objects.SpriteButton("spinButton", 289, 386);
+            this._slotMachine.addChild(this._spinButton);
+
+            this._tile1 = new objects.GameObject("blank", 74, 192);
+            this._slotMachine.addChild(this._tile1);
+
+            this._tile2 = new objects.GameObject("blank", 152, 192);
+            this._slotMachine.addChild(this._tile2);
+
+            this._tile3 = new objects.GameObject("blank", 230, 192);
+            this._slotMachine.addChild(this._tile3);
+
+            this._betLine = new objects.GameObject("bet_line", 61, 225);
+            this._slotMachine.addChild(this._betLine);
+
+            this.addChild(this._slotMachine);
             stage.addChild(this);
+
+            // add event listeners
+            this._bet1Button.on("click", this._clickBet1Button, this);
+
+            this._spinButton.on("click", this._spinButtonClick, this);
         }
 
 
@@ -37,14 +71,14 @@
 
         // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++++++++
         // Callback function / Event Handler for Back Button Click
-        private _clickBackButton(event: createjs.MouseEvent): void {
-            changeState(config.MENU_STATE);
+        private _clickBet1Button(event: createjs.MouseEvent): void {
+            console.log("bet 1");
         }
 
-        // Callback function / Event Handler for Next Button Click
-        private _clickNextButton(event: createjs.MouseEvent): void {
-            changeState(config.OVER_STATE);
+        //WORKHORSE OF THE GAME
+        private _spinButtonClick(event: createjs.MouseEvent): void {
         }
+
 
     }
 
