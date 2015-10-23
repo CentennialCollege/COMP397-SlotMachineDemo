02ef04465bb93548b68855486c10ee7ec7712316
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-10-23 16:48:53 -0400

    added gameobject object

--- COMP397-SlotMachineDemo/Scripts/core/game.ts
+++ COMP397-SlotMachineDemo/Scripts/core/game.ts
@@ -8,6 +8,8 @@
 /// <reference path="../typings/soundjs/soundjs.d.ts" />
 /// <reference path="../typings/preloadjs/preloadjs.d.ts" />
 
+/// <reference path="../objects/gameobject.ts" />
+
 /// <reference path="../objects/label.ts" />
 /// <reference path="../objects/button.ts" />
 /// <reference path="../objects/scene.ts" />
@@ -24,6 +26,7 @@ var stage: createjs.Stage;
 var stats: Stats;
 var state: number;
 var currentState: objects.Scene; // alias for our current state
+var atlas: createjs.SpriteSheet;
 
 // GAME OBJECTS
 var menu: states.Menu;
@@ -35,14 +38,62 @@ var manifest = [
     { id: "BackButton", src: "../../Assets/images/BackButton.png" },
     { id: "NextButton", src: "../../Assets/images/NextButton.png" },
     { id: "StartButton", src: "../../Assets/images/StartButton.png" },
+    { id: "background", src: "../../Assets/images/background.png" },
     { id: "yay", src: "../../Assets/audio/yay.ogg" }
 ];
+
+var data = {
+
+    "images": [
+        "../../Assets/images/atlas.png"
+    ],
+
+    "frames": [
+        [2, 2, 253, 4, 0, 0, 0],
+        [2, 8, 69, 69, 0, 0, 0],
+        [2, 79, 69, 69, 0, 0, 0],
+        [2, 150, 69, 69, 0, 0, 0],
+        [73, 8, 69, 69, 0, 0, 0],
+        [144, 8, 69, 69, 0, 0, 0],
+        [215, 8, 60, 60, 0, 0, 0],
+        [215, 70, 60, 60, 0, 0, 0],
+        [73, 79, 69, 69, 0, 0, 0],
+        [144, 79, 69, 69, 0, 0, 0],
+        [215, 132, 60, 60, 0, 0, 0],
+        [73, 194, 69, 69, 0, 0, 0],
+        [144, 150, 60, 60, 0, 0, 0],
+        [144, 212, 60, 60, 0, 0, 0],
+        [206, 194, 60, 60, 0, 0, 0]
+    ],
+
+    "animations": {
+        "bet_line": [0],
+        "banana": [1],
+        "bar": [2],
+        "bell": [3],
+        "blank": [4],
+        "cherry": [5],
+        "bet100Button": [6],
+        "bet10Button": [7],
+        "grapes": [8],
+        "orange": [9],
+        "bet1Button": [10],
+        "seven": [11],
+        "betMaxButton": [12],
+        "genericButton": [13],
+        "spinButton": [14]
+    },
+
+
+};
+
 
 function preload(): void {
     assets = new createjs.LoadQueue();
     assets.installPlugin(createjs.Sound);
     assets.on("complete", init, this);
     assets.loadManifest(manifest);
+    atlas = new createjs.SpriteSheet(data);
 }
 
 function init():void {
